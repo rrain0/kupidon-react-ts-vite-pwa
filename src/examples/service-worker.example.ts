5f32e3223a291918a5a2e9010f97fc5755717705
rrain <rrainpath@gmail.com>
2024-03-23 11:16:13 +0800

    app successfully was migrated to Vite

---
+++ src/examples/service-worker.example.ts
@@ -0,0 +1,29 @@
+/// <reference lib="webworker" />
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
+import { clientsClaim } from 'workbox-core'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
+
+declare const self: ServiceWorkerGlobalScope
+
+const selfWbManifest = self.__WB_MANIFEST
+// self.__WB_MANIFEST is the default injection point
+precacheAndRoute(selfWbManifest)
+
+// clean old res
+cleanupOutdatedCaches()
+
+let allowlist: undefined | RegExp[]
+if (import.meta.env.DEV) allowlist = [/^\/$/]
+
+// to allow work offline
+registerRoute(new NavigationRoute(
+  createHandlerBoundToURL('index.html'),
+  // @ts-ignore
+  { allowlist }
+))
+
+console.log('msgFromServiceWorker')
+console.log(selfWbManifest)
+
+self.skipWaiting()
+clientsClaim()
--- src/test.mts
+++
@@ -1,9 +0,0 @@
-
-
-console.log('test.mts start')
-
-const htmlDescription: HTMLMetaElement =
-  document.querySelector('html head meta[name=description]')!
-
-
-console.log('test.mts end')
