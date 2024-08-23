9745eab8066c9aedb5670a38170d3b68858dc9a9
rrain <rrainpath@gmail.com>
2024-08-23 15:05:44 +0800

    added age from date-fns

--- src/ui/2-pages/Profile/Summary/SummaryPage.tsx
+++ src/ui/2-pages/Profile/Summary/SummaryPage.tsx
@@ -1,11 +1,14 @@
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
+import { formatDuration, intervalToDuration, parseISO } from 'date-fns'
+import { ru, enUS } from 'date-fns/locale'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
 import { AppRoutes } from 'src/app-routes/AppRoutes'
 import { RouteBuilder } from 'src/mini-libs/route-builder/RouteBuilder'
 import { AuthRecoil } from 'src/recoil/state/AuthRecoil'
+import { LangRecoil } from 'src/recoil/state/LangRecoil'
 import Card2 from 'src/ui/0-elements/cards/Card2'
 import HeaderArrow from 'src/ui/2-pages/BowAndArrows/elements/HeaderArrow.tsx'
 import PageHeader from 'src/ui/2-pages/BowAndArrows/elements/PageHeader.tsx'
@@ -14,6 +17,7 @@ import BottomButtonBar from 'src/ui/components/BottomButtonBar/BottomButtonBar'
 import { Pages } from 'src/ui/components/Pages/Pages'
 import PageScrollbars from 'src/ui/1-widgets/Scrollbars/PageScrollbars'
 import { useUiValues } from 'src/mini-libs/ui-text/useUiText.ts'
+import { DateU } from 'src/util/date/DateU'
 import { ImagesMockData } from 'src/util/mock-data/ImagesMockData'
 import full = RouteBuilder.full
 import RootRoute = AppRoutes.RootRoute
@@ -21,14 +25,26 @@ import use = RouteBuilder.use
 
 
 
+const profileData = {
+  ava: ImagesMockData.all[7],
+  name: 'Сатору',
+  city: 'Токио',
+  birthDate: '2000-08-23T14:33:55.609+07:00',
+}
 
 
 const SummaryPage = React.memo(
   () => {
+    const lang = useRecoilValue(LangRecoil).langs[0]
     const titleText = useUiValues(TitleUiText)
     
     const auth = useRecoilValue(AuthRecoil)
     const authId = auth!.user.id
+    
+    const info = [profileData.city, DateU.age(profileData.birthDate, lang)].join(', ')
+    
+    console.log('age', DateU.age('2000-08-23T14:33:55.609+07:00', lang))
+    console.log('ymd', DateU.yearsMonthsDaysFromBirthDate('2000-08-23T14:33:55.609+07:00', lang))
     
     return (
       <>
@@ -39,10 +55,10 @@ const SummaryPage = React.memo(
               
               <Link to={RootRoute.profile.id.userId[use](authId).profile[full]()}>
                 <Card2 css={cardStyle}>
-                  <Ava src={ImagesMockData.all[7]}/>
-                  <Name>Name</Name>
+                  <Ava src={profileData.ava}/>
+                  <Name>{profileData.name}</Name>
                   <Eye>Eye</Eye>
-                  <Info>Info</Info>
+                  <Info>{info}</Info>
                   <Edit>Edit</Edit>
                 </Card2>
               </Link>
---
+++ src/util/date/DateU.ts
@@ -0,0 +1,66 @@
+import { formatDuration, intervalToDuration, Locale } from 'date-fns'
+import { enUS, ru } from 'date-fns/locale'
+import { Lang } from 'src/util/lang/Lang'
+
+/*
+parse: https://date-fns.org/v2.0.0-alpha.6/docs/parse
+
+example get age:
+formatDuration(
+  intervalToDuration({
+    start: parseISO('1997-11-22T02:00:00'),
+    end: new Date(),
+  }),
+  {
+    format: ['years'],
+    locale: enUS,
+  }
+))
+
+*/
+
+
+export namespace DateU {
+  
+  const appLangToDateFnsLocale: Record<Lang.Supported, Locale> = {
+    'en-US': enUS,
+    'ru-RU': ru,
+  }
+  
+  
+  /**
+   * @param birthDate date in format like '2000-08-23T14:33:55.609+07:00'
+   * @returns '23 years', '23 года'
+   * */
+  export const age = (birthDate: string, appLang: Lang.Supported) => {
+    return formatDuration(
+      intervalToDuration({
+        start: new Date(birthDate),
+        end: new Date(),
+      }),
+      {
+        format: ['years'],
+        locale: appLangToDateFnsLocale[appLang],
+      }
+    )
+  }
+  
+  
+  /**
+   * @param birthDate date in format like '2000-08-23T14:33:55.609+07:00'
+   * @returns '23 years 11 months 30 days', '23 года 11 месяцев 30 дней'
+   * */
+  export const yearsMonthsDaysFromBirthDate = (birthDate: string, appLang: Lang.Supported) => {
+    return formatDuration(
+      intervalToDuration({
+        start: new Date(birthDate),
+        end: new Date(),
+      }),
+      {
+        format: ['years', 'months', 'days'],
+        locale: appLangToDateFnsLocale[appLang],
+      }
+    )
+  }
+  
+}
